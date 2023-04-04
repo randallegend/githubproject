003ea786b8f440e79f8b863b22cb1de90653229d
Hanz <hanznikkolmaas@gmail.com>
2023-04-04 10:43:01 +0800

    randall

--- app/src/main/java/com/example/lupetproject/MainActivity.java
+++ app/src/main/java/com/example/lupetproject/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String hanz = "pangit mo randall bobo shunga";
+        String hanz = "pangit mo randall";
     }
 }
